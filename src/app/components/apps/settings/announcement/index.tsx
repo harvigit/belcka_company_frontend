7b7f4ac4c6b4a54d23052ee55b8a85e835a98aa2
urvish <urvishgodhani8549@gmail.com>
2025-11-05 12:27:38 +0530

    small change

--- src/app/components/apps/settings/announcement/index.tsx
+++ src/app/components/apps/settings/announcement/index.tsx
@@ -272,7 +272,6 @@ export default function AnnouncementsList({
                                         size="small"
                                         startIcon={<IconFileDownload />}
                                       >
-                                        Download
                                       </Button>
                                     )}
                                   </Box>
