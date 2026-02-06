fe9d52297e14fa1012cd2e8c8480e05c91d788de
urvish <urvishgodhani8549@gmail.com>
2026-02-06 15:00:47 +0530

    small change

--- src/app/components/apps/user-profile-setting/payments/index.tsx
+++ src/app/components/apps/user-profile-setting/payments/index.tsx
@@ -23,33 +23,35 @@ const Payments: React.FC<PayslipListingProps> = ({
   const [activeTab, setActiveTab] = useState(0);
 
   return (
-    <PermissionGuard permission={userId ? "Users" : "Payments"}>
-      <Tabs
-        className="user-tabs"
-        aria-label="Sidebar Tabs"
-        value={activeTab}
-        onChange={(_, val) => setActiveTab(val)}
-        sx={{
-          textTransform: "none",
-          borderRadius: "10px",
-          px: 3,
-          py: 0.5,
-          fontSize: "14px",
-          fontWeight: 600,
-          transition: "all 0.3s ease",
-        }}
-      >
-        <Tab label="Payslips" sx={{ fontWeight: 600 }} />
-        <Tab label="Payments" sx={{ fontWeight: 600 }} />
-        <Tab label="Invoices" sx={{ fontWeight: 600 }} />
-      </Tabs>
+    <PermissionGuard permission={userId  && !Number.isNaN(userId) ? "Users" : "Payments"}>
+      <Box>
+        <Tabs
+          className="user-tabs"
+          aria-label="Sidebar Tabs"
+          value={activeTab}
+          onChange={(_, val) => setActiveTab(val)}
+          sx={{
+            textTransform: "none",
+            borderRadius: "10px",
+            px: 3,
+            py: 0.5,
+            fontSize: "14px",
+            fontWeight: 600,
+            transition: "all 0.3s ease",
+          }}
+        >
+          <Tab label="Payslips" sx={{ fontWeight: 600 }} />
+          <Tab label="Payments" sx={{ fontWeight: 600 }} />
+          <Tab label="Invoices" sx={{ fontWeight: 600 }} />
+        </Tabs>
 
-      <Divider />
+        <Divider />
 
-      <Box mt={2}>
-        {activeTab === 0 && <PayslipsList userId={userId} isShow={isShow} />}
-        {activeTab === 1 && <PaymentsList userId={userId} isShow={isShow} />}
-        {activeTab === 2 && <InvoicesList userId={userId} isShow={isShow} />}
+        <Box mt={2}>
+          {activeTab === 0 && <PayslipsList userId={userId} isShow={isShow} />}
+          {activeTab === 1 && <PaymentsList userId={userId} isShow={isShow} />}
+          {activeTab === 2 && <InvoicesList userId={userId} isShow={isShow} />}
+        </Box>
       </Box>
     </PermissionGuard>
   );
