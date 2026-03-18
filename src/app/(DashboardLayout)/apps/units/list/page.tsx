61c16abb16c3e52395d7c467271057addbb23075
urvish <urvishgodhani8549@gmail.com>
2026-03-18 12:06:54 +0530

    build changes

--- src/app/(DashboardLayout)/apps/units/list/page.tsx
+++ src/app/(DashboardLayout)/apps/units/list/page.tsx
@@ -1,15 +1,20 @@
-import React from "react";
+"use client";
+
 import PageContainer from "@/app/components/container/PageContainer";
 import BlankCard from "@/app/components/shared/BlankCard";
 import UnitList from "@/app/components/apps/units/list";
+import { useState } from "react";
 
 const unitListing = () => {
+  const [open, setOpen] = useState(false);
+
   return (
     <PageContainer title="Unit List" description="this is Unit List">
       <BlankCard>
-        <UnitList openDrawer={true} onClose={() => {}} />
+        <UnitList openDrawer={open} onClose={() => setOpen(false)} />;
       </BlankCard>
     </PageContainer>
   );
 };
+
 export default unitListing;
