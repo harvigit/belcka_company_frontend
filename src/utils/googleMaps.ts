14d08ce0cbab837c1d1dbc4b76234b9421e2af3b
arpit-golakiya <arpitgolakiya6@gmail.com>
2026-07-03 11:04:48 +0530

    small changes

--- src/utils/googleMaps.ts
+++ src/utils/googleMaps.ts
@@ -1,11 +1,13 @@
-import type { Libraries, UseLoadScriptOptions } from '@react-google-maps/api';
+import type { Libraries } from '@react-google-maps/api';
 
 export const GOOGLE_MAP_LIBRARIES: Libraries = ['places', 'drawing', 'geometry'];
 
-export const GOOGLE_MAPS_SHARED_LOADER_OPTIONS: Pick<
-    UseLoadScriptOptions,
-    'id' | 'language' | 'region' | 'libraries'
-> = {
+export const GOOGLE_MAPS_SHARED_LOADER_OPTIONS: {
+    id: string;
+    language: string;
+    region: string;
+    libraries: Libraries;
+} = {
     id: 'script-loader',
     language: 'en',
     region: 'US',
